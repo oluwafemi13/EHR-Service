1d6c2afdb12294b3980a0356b67f763a5f24309d
IKECHUKWU <sundayjabikem@gmail.com>
2023-08-06 04:04:47 +0100

    setting up core

---
+++ Solution/EHR.Core/Repositories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHR.Core
+{
+    internal class Repositories
+    {
+    }
+}
