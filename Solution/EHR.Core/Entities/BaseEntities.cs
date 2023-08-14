6918a0b45012e98bbe8b18fe312e72dd88d55e8c
IKECHUKWU <sundayjabikem@gmail.com>
2023-08-14 13:10:31 +0100

    update

--- Solution/EHR.Core/Entities/BaseEntities.cs
+++ Solution/EHR.Core/Entities/BaseEntities.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EHR.Core.Entities
 {
-    internal class Entities
+    public class Entities
     {
     }
 }
--- Solution/EHR.Core/Repositories/IBaseRepository.cs
+++ Solution/EHR.Core/Repositories/IBaseRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EHR.Core.Repositories
 {
-    internal class Repositories
+    public interface Repositories
     {
     }
 }
