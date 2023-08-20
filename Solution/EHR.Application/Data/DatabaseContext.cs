866de77a4da686890472e7c6831b3e43dd326700
Iwuchukwu Oluwafemi Ikechukwu <sundayjabikem@gmail.com>
2023-08-20 15:58:21 +0100

    Added Test Project

---
+++ Solution/EHR.Application/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.EntityFrameworkCore.Internal;
+using EHR.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHR.Application.Data
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options): base(options) { }
+
+
+       
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Staff> Staffs { get; set;}
+
+    }
+}
